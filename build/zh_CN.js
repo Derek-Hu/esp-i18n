761aab14a23f2f0b4d5ec6595803e065cec14b12
胡本绿 <benlv.hu@dianrong.com>
2019-12-10 17:19:12 +0800

    Update

--- build/zh_CN.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "be-sorry-menu-limits-of-authority-contact": "抱歉你没有菜单权限,请联系管理员。",
-  "waste-number-coffee": "浪费多少咖啡里的撒",
-  "ha-ha-ha": "哈哈哈",
-  "to-punch-cards": "\n        来打卡\n      "
-}
--- src/main.js
+++ src/main.js
@@ -4,24 +4,14 @@ const puppeteer = require('puppeteer');
 const esprima = require('esprima');
 const Utils = require('./utils');
 
-const folders = ['src/pages/error'];
-const baseFolder = '/Users/hubenlv/workspaces/rc-frontend';
-const localToolsPath = '~/locale-tools';
-
-const target = path.resolve(process.cwd(), 'build');
-
 const Types = {
     JSXText: 'JSXText',
     JSXAttribute: 'JSXAttribute',
 };
-function sleep(seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
-}
 function isChineaseText(value) {
     return /[\u4e00-\u9fa5]/.test(value);
 }
 function getJSFileList(root) {
-    // console.log('root', root);
     let res = [];
     let files = fs.readdirSync(root);
 
@@ -31,15 +21,21 @@ function getJSFileList(root) {
 
         if (!stat.isDirectory() && /\.js$/.test(pathname)) {
             res.push(pathname);
-        } else {
-            res = res.concat(getFileList(pathname));
+        } else if (stat.isDirectory()) {
+            console.log('pathname', pathname)
+            res = res.concat(getJSFileList(pathname));
         }
     });
-    // console.log(res);
     return res;
 }
 
-(async () => {
+module.exports = async (params) => {
+
+    const folders = params.folders || [];
+    const baseFolder = process.cwd();
+    const localToolsPath = params.localTools || '~/locale-tools';
+    const target = path.resolve(process.cwd(), params.target);
+
     function isChinaCall(node, meta, file, nameMapping) {
         const Names = {
             formatMessage: nameMapping['formatMessage'] || 'formatMessage',
@@ -65,7 +61,7 @@ function getJSFileList(root) {
     }
 
     const browser = await puppeteer.launch({
-        headless: false
+        headless: params.headless !== false
     });
 
     process.on('uncaughtException', async function () {
@@ -90,7 +86,7 @@ function getJSFileList(root) {
         try {
             const datas = await page.evaluate(() => {
                 function formatWord(word) {
-                    return word.toLowerCase().replace(/[,.']/g, '').trim().replace(/\s/g, '-');
+                    return word.toLowerCase().replace(/[,.:']/g, '').trim().replace(/\s/g, '-');
                 }
 
                 const translationEle = document.querySelector("p.ordinary-output.target-output");
@@ -114,8 +110,6 @@ function getJSFileList(root) {
                         return '';
                     }).filter(v => v);
 
-                    console.log('allFirstMeans', allFirstMeans);
-                    debugger;
                     id = allFirstMeans.reduce((means, word, index) => {
                         if (index >= maxWords) {
                             return means;
@@ -124,14 +118,11 @@ function getJSFileList(root) {
                         return means;
                     }, []).join('-');
                 }
-                debugger;
                 return {
                     id: id,
                     english: translation
                 };
             }, {});
-
-            console.log('datas', datas);
 
             chinaJSON[datas.id] = words;
             englishJSON[datas.id] = datas.english;
@@ -157,7 +148,6 @@ function getJSFileList(root) {
             return names;
         }, []).join('-');
         const id = await translation(value);
-        console.log('id: ', id);
         return `${name.replace(/\.js$/, '')}.${id ? id : value}`;
     }
 
@@ -169,86 +159,86 @@ function getJSFileList(root) {
 
     await asyncForEach(folders, async folder => {
         const jsFiles = getJSFileList(path.resolve(baseFolder, folder));
-        // console.log(jsFiles);
         await asyncForEach(jsFiles, async file => {
 
             const entries = [];
             let source = fs.readFileSync(file, 'UTF8').replace(/@/g, '');
-            // console.log(source);
             let hasImported = false;
             let importMeta = null;
             const importToolNames = [];
             const nameMapping = {};
 
-            esprima.parseModule(source, { jsx: true, tokens: true }, function (node, meta) {
-                if (!hasImported) {
-                    hasImported = (node.type === 'ImportDeclaration' && node.source.value === localToolsPath);
-                    if (hasImported) {
-                        // console.log(node);
-                        node.specifiers.forEach(spec => {
-                            nameMapping[spec.imported.name] = spec.local.name;
-                            importToolNames.push(`${spec.imported.name}${spec.imported.name !== spec.local.name ? ` as ${spec.local.name}` : ''}`);
-                        });
+            try {
 
-                        importMeta = {
-                            start: meta.start.offset,
-                            end: meta.end.offset,
+                esprima.parseModule(source, { jsx: true, tokens: true }, function (node, meta) {
+                    if (!hasImported) {
+                        hasImported = (node.type === 'ImportDeclaration' && node.source.value === localToolsPath);
+                        if (hasImported) {
+                            node.specifiers.forEach(spec => {
+                                nameMapping[spec.imported.name] = spec.local.name;
+                                importToolNames.push(`${spec.imported.name}${spec.imported.name !== spec.local.name ? ` as ${spec.local.name}` : ''}`);
+                            });
+
+                            importMeta = {
+                                start: meta.start.offset,
+                                end: meta.end.offset,
+                            }
                         }
                     }
-                }
 
-                const call = isChinaCall(node, meta, file, nameMapping);
-                if (call) {
-                    entries.push(call);
-                }
-            });
-            // console.log(entries);
-
-            await asyncForEach(entries, async entry => {
-                entry.id = await getId(entry.value, entry.file);
-            });
-            // 动态计算 import { formatMessage, FormattedMessage } from ${localToolsPath};
-            const hasJSToolImport = entries.some(entry => !entry.isComponent);
-            const hasCompToolImport = entries.some(entry => entry.isComponent);
-
-            const metas = {
-                formatMessage: hasJSToolImport,
-                FormattedMessage: hasCompToolImport,
-            }
-            Object.keys(metas).forEach((key) => {
-                if (metas[key]) {
-                    console.log(key, nameMapping[key], nameMapping);
-                    let statement = null;
-                    if (nameMapping[key] && (nameMapping[key] !== key)) {
-                        statement = `${key} as ${nameMapping[key]}`;
-                    } else {
-                        statement = key;
+                    const call = isChinaCall(node, meta, file, nameMapping);
+                    if (call) {
+                        entries.push(call);
                     }
-                    if (statement && !importToolNames.includes(statement)) {
-                        importToolNames.push(statement);
-                    }
+                });
+
+                await asyncForEach(entries, async entry => {
+                    entry.id = await getId(entry.value, entry.file);
+                });
+                // 动态计算 import { formatMessage, FormattedMessage } from ${localToolsPath};
+                const hasJSToolImport = entries.some(entry => !entry.isComponent);
+                const hasCompToolImport = entries.some(entry => entry.isComponent);
+
+                const metas = {
+                    formatMessage: hasJSToolImport,
+                    FormattedMessage: hasCompToolImport,
                 }
-            })
-            let finalImport;
-            if (importToolNames.length) {
-                finalImport = `import { ${importToolNames.join(', ')} } from '${localToolsPath}';\n`;
-            }
+                Object.keys(metas).forEach((key) => {
+                    if (metas[key]) {
+                        let statement = null;
+                        if (nameMapping[key] && (nameMapping[key] !== key)) {
+                            statement = `${key} as ${nameMapping[key]}`;
+                        } else {
+                            statement = key;
+                        }
+                        if (statement && !importToolNames.includes(statement)) {
+                            importToolNames.push(statement);
+                        }
+                    }
+                })
+                let finalImport;
+                if (importToolNames.length) {
+                    finalImport = `import { ${importToolNames.join(', ')} } from '${localToolsPath}';\n`;
+                }
 
-            const sortedEntries = entries.sort((a, b) => { return b.end - a.end });
-            if (hasImported && finalImport) {
-                sortedEntries.push(Object.assign({}, importMeta, {
-                    getReplacement: () => finalImport
-                }));
-            }
+                const sortedEntries = entries.sort((a, b) => { return b.end - a.end });
+                if (hasImported && finalImport) {
+                    sortedEntries.push(Object.assign({}, importMeta, {
+                        getReplacement: () => finalImport
+                    }));
+                }
 
-            sortedEntries.forEach(n => {
-                source = source.slice(0, n.start) + n.getReplacement(n.id) + source.slice(n.end);
-            });
+                sortedEntries.forEach(n => {
+                    source = source.slice(0, n.start) + n.getReplacement(n.id) + source.slice(n.end);
+                });
 
-            if (finalImport && !hasImported) {
-                source = finalImport + source;
+                if (finalImport && !hasImported) {
+                    source = finalImport + source;
+                }
+                Utils.writeSync(file, source);
+            } catch (e) {
+                console.log(`解析文件失败：${file}`);
             }
-            Utils.writeSync(path.resolve(target, path.relative(baseFolder, file)), source);
         });
     });
 
@@ -256,4 +246,4 @@ function getJSFileList(root) {
     Utils.writeSync(path.resolve(target, 'en_US.js'), `export default ${JSON.stringify(englishJSON, null, 2)}`);
 
     await browser.close();
-})();
+}
