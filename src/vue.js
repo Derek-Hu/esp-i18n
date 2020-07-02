35bc20405978c9869dcc8eb6bafff4496736d4d4
胡本绿 <benlv.hu@dianrong.com>
2020-07-02 16:17:17 +0800

    支持Vue

--- src/vue.js
+++ src/vue.js
@@ -1,15 +1,144 @@
-// const fs = require('fs');
-// const path = require('path');
-// const Utils = require('./utils');
 const translateByRemote = require('./translateByRemote');
+const babelParser = require("@babel/parser");
+const BabelOption = require('./babel');
+const traverse = require("@babel/traverse").default;
 
 const translation = translateByRemote.translate;
 
+const cammelCase = (id) => {
+    if (!id) {
+        return id;
+    }
+    const parts = id.split('-');
+    return parts.reduce((total, part, index) => {
+        if (index === 0) {
+            total.push(part);
+            return total;
+        }
+        total.push(part[0].toUpperCase() + part.substring(1));
+        return total;
+    }, []).join('');
+}
+const getVueScriptContent = (source) => {
+    if (!source) {
+        return source;
+    }
+    const matchs = source.match(/<script>((.*\n)*)<\/script>/);
+    if (matchs && matchs[1]) {
+        return matchs[1];
+    }
+    return source;
+}
+const updateModifedScripts = (source, newSource) => {
+    const scripts = getVueScriptContent(source);
+    return source.replace(scripts, newSource);
+}
+
+const parseVueData = (source, i18n, IDName) => {
+    const PluginOptions = BabelOption(false);
+    const astTree = babelParser.parse(source, PluginOptions);
+    const actions = [];
+    traverse(astTree, {
+        ExportDefaultDeclaration(_node) {
+            const declaration = _node.node.declaration;
+            if (declaration.type === 'ObjectExpression') {
+                const properties = declaration.properties;
+                if (properties && properties.length) {
+                    const dataMethod = properties.find(prop => {
+                        return prop.type === 'ObjectMethod' && prop.key && prop.key.type === 'Identifier' && prop.key.name === 'data';
+                    });
+                    if (!dataMethod) {
+                        actions.push({
+                            start: properties[properties.length - 1].start,
+                            end: properties[properties.length - 1].end,
+                            isInsert: true,
+                            getReplacement: () => `data(){return { ${IDName}: ${JSON.stringify(i18n, null, 2)}}},`
+                        });
+                        return;
+                    }
+                    const body = dataMethod.body ? dataMethod.body.body : null;
+
+                    const returnStatment = body ? body.find(statement => statement.type === 'ReturnStatement') : null;
+                    if (!body.length) {
+                        actions.push({
+                            start: dataMethod.body.start,
+                            end: dataMethod.body.end,
+                            isInsert: true,
+                            getReplacement: () => `return { ${IDName}: ${JSON.stringify(i18n, null, 2)}}`
+                        });
+                        return;
+                    } else if (!returnStatment) {
+                        actions.push({
+                            start: body[body.length - 1].start,
+                            end: body[body.length - 1].end,
+                            isInsert: true,
+                            getReplacement: () => `return { ${IDName}: ${JSON.stringify(i18n, null, 2)}}`
+                        });
+                        return;
+                    }
+                    if (returnStatment.argument.type === 'ObjectExpression') {
+                        if (returnStatment.argument.properties && returnStatment.argument.properties.length) {
+                            const hasLabels = returnStatment.argument.properties.find(p => p.type === 'ObjectProperty' && p.key.name === IDName);
+                            if (hasLabels) {
+                                if (hasLabels.value.type === 'ObjectExpression') {
+                                    if (hasLabels.value.properties.length) {
+                                        console.log('000')
+                                        actions.push({
+                                            start: hasLabels.value.properties[0].start,
+                                            end: hasLabels.value.properties[0].end,
+                                            isInsert: true,
+                                            getReplacement: () => `,${Object.keys(i18n).map(key => `${key}:${i18n[key]}, `).join(',\n')}`
+                                        });
+                                    }
+                                }
+                            } else {
+                                console.log('001')
+                                actions.push({
+                                    start: returnStatment.argument.properties[0].start,
+                                    end: returnStatment.argument.properties[0].end,
+                                    isInsert: true,
+                                    getReplacement: () => `\n,Labels: ${JSON.stringify(i18n, null, 2)}`
+                                });
+                            }
+                        } else {
+                            console.log('002')
+                            actions.push({
+                                start: returnStatment.argument.start,
+                                end: returnStatment.argument.end,
+                                isInsert: true,
+                                getReplacement: () => `\nLabels: ${JSON.stringify(i18n, null, 2)},`
+                            });
+                        }
+                    }
+                }
+            }
+        },
+    });
+
+    const sortedEntries = actions.sort((a, b) => { return b.end - a.end });
+
+    sortedEntries.forEach(n => {
+        if (n.isInsert) {
+            source = source.slice(0, n.end) + n.getReplacement() + source.slice(n.end);
+        } else {
+            source = source.slice(0, n.start) + n.getReplacement() + source.slice(n.end);
+        }
+    });
+
+    return {
+        source,
+        isUpdated: actions.length
+    };
+}
+
+const projectIds = {};
+
 module.exports = async (filepath, content, launchOptions) => {
-    // console.log(filepath);
+    console.log(filepath);
     let lines = content.split('\n');
 
-    const words = {};
+    const IDName = 'Labels';
+
     let isStart = false;
     let isEnd = false;
     let isComment = false;
@@ -17,9 +146,13 @@ module.exports = async (filepath, content, launchOptions) => {
     const TranslationContainer = {};
     const duplicateKeys = {};
 
+    const labels = {};
     const newLines = [];
-
+    let processIdx = 0;
     for (line of lines) {
+
+        processIdx++;
+        console.log(`${processIdx}/${lines.length}`);
 
         if (currentIsCommentEnd) {
             currentIsCommentEnd = false;
@@ -45,49 +178,48 @@ module.exports = async (filepath, content, launchOptions) => {
             newLines.push(line);
             continue;
         }
-        const lineWords = line.match(/[\u4e00-\u9fa5，。]+[a-z0-9A-Z]*[\u4e00-\u9fa5，。]+/g);
-        if (lineWords && lineWords.length) {
-            lineWords.forEach(element => {
-                words[element] = true;
-            });
-            if (lineWords.length === 1) {
-                const reg = new RegExp('\\w+="' + lineWords[0]);
-                const attrMatch = line.match(reg);
-                if (attrMatch && attrMatch[0]) {
+        const lineWords = line.match(/[a-z0-9()\sA-Z?]*[\u4e00-\u9fa5，（）？。]+[a-z0-9A-Z]*[\u4e00-\u9fa5，（）？。]+[a-z0-9()\sA-Z?]*/g);
 
-                    // console.log('line:', reg, line, attrMatch, line.replace(attrMatch[0], ':' + attrMatch[0]));
-                    line = line.replace(attrMatch[0], ':' + attrMatch[0]);
-                    words[lineWords[0]] = line;
+        console.log('lineWords', lineWords);
+        if (lineWords && lineWords.length) {
+            if (lineWords.length === 1) {
+                
+                const id = await translation(launchOptions, lineWords[0], 'en', projectIds[lineWords[0]], TranslationContainer, duplicateKeys);
+                const vid = cammelCase(id);
+
+                projectIds[lineWords[0]] = vid;
+
+                let reg = new RegExp('(\\w+=)"' + lineWords[0] + '"');
+                let attrMatch = line.match(reg);
+                if (!attrMatch) {
+                    reg = new RegExp(`(:\\w+=)"'` + lineWords[0] + `'"`);
+                    attrMatch = line.match(reg);
                 }
-                const id = await translation(launchOptions, lineWords[0], 'en', null, TranslationContainer, duplicateKeys);
-                words[lineWords[0]] = 'id=' + id + ' ' + words[lineWords[0]];
-                console.log(lineWords[0], words[lineWords[0]])
-            } else {
-                console.log('greate: ', filepath, lineWords);
+                if (attrMatch && attrMatch[0]) {
+                    line = line.replace(attrMatch[0], `:${attrMatch[1]}="${IDName}.${vid}"`);
+                    console.log('attrMatch', line, attrMatch);
+                }
+
+                if (!attrMatch) {
+                    line = line.replace(lineWords[0], `{{${IDName}.${vid}}}`);
+                }
+
+                labels[vid] = lineWords[0];
             }
         }
         newLines.push(line);
     }
 
-    if (Object.keys(words).length) {
-        return '<templte>\nLabels: {\n' + Object.keys(words).map(w => `'${w}${words[w] === true ? '' : '=' + words[w]}'`).join(',\n') + '\n}\n</templte>\n' + newLines.join('\n');
+    const newSource = newLines.join('\n');
+    if (Object.keys(labels).length) {
+        const scripts = getVueScriptContent(content);
+        const { source: modifiedScripts, isUpdated } = parseVueData(scripts, labels, IDName);
+        const modified = updateModifedScripts(newSource, modifiedScripts);
+        console.log(modifiedScripts);
+        if (isUpdated) {
+            return modified;
+        }
+        return '<templte>\nLabels: {\n' + Object.keys(labels).map(w => `${w}=${labels[w]}`).join(',\n') + '\n}\n</templte>\n' + modified;
     }
-    // console.log('content', content)
     return content;
 }
-
-// module.exports = (filepath, content) => {
-//     console.log(filepath);
-//     const matchs = content.match(/<template[^>]*>((.*\n)*)<\/template>/);
-//     const validContent = matchs && matchs[1];
-
-//     const china = validContent.match(/[\u4e00-\u9fa5]+[a-z0-9A-Z]*[\u4e00-\u9fa5]+/g);
-//     if (china && china.length) {
-//         const total = china.reduce((total, word)=>{
-//             total[word] = true;
-//             return total;
-//         }, {});
-//         return '<templte>\nLabels: {\n' + Object.keys(total).map(w => `'${w}'`).join(',\n') + '\n}\n</templte>\n' + content;
-//     }
-//     return content;
-// }
