2019e3d1b25c2e8c882f7c83640c0699f6c00448
胡本绿 <benlv.hu@dianrong.com>
2020-07-13 10:10:26 +0800

    更新代码

--- test/code/vue/data-empty.vue
+++ test/code/vue/data-empty.vue
@@ -7,6 +7,8 @@
     <p>建议提前准备如下资料，以便快速完成填写</p>
     <span :title="'发票照片'"></span>
     <div placeholder="请输入贸易合同编号" />
+    <div placeholder='请输入贸易合同编号' />
+    <div placeholder = "请输入贸易合同编号" />
     <span :title="'发票照片'"></span>
     <div :placeholder=" isTrue ? '请输入贸易合同编号': '呵呵呵'"></div>
     <div :placeholder=" isTrue ? '呵呵呵2': '呵呵呵'"></div>
