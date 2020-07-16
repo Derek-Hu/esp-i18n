774b1efcbbf2c9f687bb2120b9a049adbc6fb6d5
胡本绿 <benlv.hu@dianrong.com>
2020-07-16 14:42:31 +0800

    完善测试

--- __tests__/translate.js
+++ __tests__/translate.js
@@ -5,7 +5,7 @@ import parseParam from '~/param';
 
 const toolPath = '~/locale-tools';
 const encode = 'UTF8';
-
+const targetLocale = 'test/locale-t';
 jest.setTimeout(30000);
 
 describe('正确增加ID', () => {
@@ -14,7 +14,7 @@ describe('正确增加ID', () => {
         folders: ['test/code/translate'],
         localTools: toolPath,
         // translate: ['th'],
-        target: 'test/locale-t',
+        target: targetLocale,
         srcCopyFolder: 'dist',
     };
 
@@ -24,6 +24,37 @@ describe('正确增加ID', () => {
     beforeAll(async () => {
         await i18n(pamras);
     });
+
+    it('对象属性动态化[fm]', async () => {
+        const zhAfter = parseLocalJson(fs.readFileSync(path.resolve(parsed.target, 'zh.js'), encode));
+        const enAfter = parseLocalJson(fs.readFileSync(path.resolve(parsed.target, 'en.js'), encode));
+
+        const zhBefore = parseLocalJson(fs.readFileSync(path.resolve(process.cwd(), targetLocale, 'zh.js'), encode));
+        const enBefore = parseLocalJson(fs.readFileSync(path.resolve(process.cwd(), targetLocale, 'en.js'), encode));
+
+        const zhSize = Object.keys(zhAfter).length;
+        const enSize = Object.keys(enAfter).length;
+
+        expect(zhSize === enSize).toBe(true);
+
+        const allBefore = {
+            ...zhBefore,
+            ...enBefore,
+        }
+        expect(Object.keys(allBefore).every(before => {
+            return ((before in zhAfter) && (before in enAfter));
+        })).toBe(true);
+
+        expect(zhAfter['welcome']).toBe('欢迎 ');
+        expect(zhAfter['how-are-you-1']).toBe(' 你好吗  ');
+        expect(zhAfter['how-are-you-2']).toBe('你好吗  ');
+        expect(zhAfter['how-are-you-3']).toBe('你好吗 ');
+        expect(zhAfter['status-1']).toBe('状态');
+
+        expect(enBefore['status-1']).toBe('state');
+        expect(enBefore['hello']).toBe('Hello');
+    });
+
 
 
 });
